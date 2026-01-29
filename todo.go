6df702434eb35fe2d871294d4ed9063b5333d99a
mohamed-abdelahi-haibelty <mohamedabdelahi2002@gmail.com>
2026-01-29 10:24:05 +0000

    feat(todo): add CRUD functionality

---
+++ todo.go
@@ -0,0 +1,73 @@
+// todo.go
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Todo struct {
+	Title string
+	Completed bool
+	CreatedAt time.Time
+	CompletedAt *time.Time
+}
+
+type Todos []Todo
+
+func (todos *Todos) Add(title string){
+     todo := Todo{
+        Title: title,
+		Completed: false,
+		CompletedAt: nil,
+		CreatedAt: time.Now(),
+	 }
+	 *todos = append(*todos, todo)
+}
+
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos){
+		err := errors.New("Invalid index")
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (todos *Todos) Delete(index int) error{
+	 if err := todos.validateIndex(index); err != nil{
+		return err
+	 }
+	 *todos = append((*todos)[:index], (*todos)[index + 1:]...)
+	 return nil
+}
+
+func (todos *Todos) Toggle(index int) error{
+	if err := todos.validateIndex(index); err != nil{
+		return err
+	}
+
+	todo := &(*todos)[index]
+
+	if !todo.Completed {
+		now := time.Now()
+		todo.CompletedAt = &now
+	}else{
+		todo.CompletedAt = nil
+	}
+
+	todo.Completed = !todo.Completed
+    
+	return nil
+}
+
+func (todos *Todos) Edit(index int, title string,) error{
+    if err := todos.validateIndex(index); err != nil{
+		return err
+	}
+
+	(*todos)[index].Title = title
+    
+	return nil
+}
